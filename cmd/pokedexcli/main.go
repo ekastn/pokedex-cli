c9dc6aa3f94e3e44fd3d79a659d670df43373fab
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-07-13 14:31:37 +0700

    Add start up text output

--- cmd/pokedexcli/main.go
+++ cmd/pokedexcli/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/eka-septian/pokedex-cli/internal/repl"
 )
 
 func main() {
+	dat, err := os.ReadFile("assets/pokedex.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(dat))
+
 	repl.SartRepl()
 }
